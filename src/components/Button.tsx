c608c7e1965a23a6a5149a34506434e7839ca8b0
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2022-07-23 15:47:53 -0300

    Tela de login finalizada (Parte Visual)

---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+import { Button as ButtonNativebase, IButtonProps, Heading } from 'native-base';
+import React from 'react';
+
+
+type Props  = IButtonProps & {
+    title: string
+}
+export function Button({ title, ...rest }: Props) {
+  return (
+    <ButtonNativebase 
+        bg="green.700"
+        h={14}
+        fontSize="sm"
+        rounded="sm"
+        _pressed={{
+            bg:"green.500"
+        }}
+    {...rest}
+    >
+        <Heading color="white" fontSize="sm">
+            {title}
+        </Heading>
+    </ButtonNativebase>
+
+  );
+}
---
+++ src/screens/Home.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { HStack, IconButton, VStack, useTheme, Text, Heading } from 'native-base';
+import { SignOut } from 'phosphor-react-native'
+
+import Logo from '../assets/iconHelpDesk.svg';
+
+export function Home() {
+    const { colors } = useTheme();
+
+
+  return (
+    <VStack flex={1} pb={6} bg="gray.700">
+        <HStack 
+        w='full'
+        justifyContent='space-between'
+        alignItems='center'
+        bg='gray.600'
+        pt={12}
+        pb={5}
+        px={6}
+        >
+            <Logo />
+            <IconButton 
+                icon={<SignOut size={26} color={colors.gray[300]} />}
+            />
+
+
+        </HStack>
+
+        <VStack flex={1} px={6}>
+          <HStack w='full' mt={8} mb={4} justifyContent='space-between' alignItems='center'>
+
+            <Heading color='gray.100'>
+                Meus Chamados
+            </Heading>
+
+            <Text color="gray.200">
+                 3
+             </Text>
+          </HStack>
+        </VStack>
+
+    <HStack>
+        
+    </HStack>
+
+     </VStack>
+  );
+}
